928cc72ed2f55f9172101486922a9e411abb4dcd
Ian Simon <isimon@umich.edu>
2019-04-01 17:25:05 -0400

    Start of the log in page

--- HotelSystem/Form1.Designer.vb
+++ HotelSystem/Form1.Designer.vb
@@ -22,10 +22,89 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.UsernameBox = New System.Windows.Forms.TextBox()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'UsernameBox
+        '
+        Me.UsernameBox.Location = New System.Drawing.Point(261, 96)
+        Me.UsernameBox.Name = "UsernameBox"
+        Me.UsernameBox.Size = New System.Drawing.Size(212, 20)
+        Me.UsernameBox.TabIndex = 0
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(261, 123)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PasswordBox.Size = New System.Drawing.Size(212, 20)
+        Me.PasswordBox.TabIndex = 1
+        Me.PasswordBox.UseSystemPasswordChar = True
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(398, 149)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoginButton.TabIndex = 2
+        Me.LoginButton.Text = "Enter"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(202, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Log in"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(202, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(202, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.PasswordBox)
+        Me.Controls.Add(Me.UsernameBox)
+        Me.Name = "Form1"
+        Me.Text = "Hotel Master"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents UsernameBox As TextBox
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents LoginButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
