80d015a936e10cea8783565d587f514f4cf424fe
Ian Simon <isimon@umich.edu>
2019-04-01 18:15:47 -0400

    New changes, login information is iSimon/ TestPass1
    All items on screen disappear upon button click

---
+++ HotelSystem/LoginPage.vb
@@ -0,0 +1,15 @@
+﻿Public Class LoginPage
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        If UsernameBox.Text = "iSimon" Then
+            If PasswordBox.Text = "TestPass1" Then
+                Label1.Visible = False
+                Label2.Visible = False
+                Label3.Visible = False
+                UsernameBox.Visible = False
+                PasswordBox.Visible = False
+                LoginButton.Visible = False
+            End If
+        End If
+
+    End Sub
+End Class
--- HotelSystem/My Project/Application.Designer.vb
+++ HotelSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HotelSystem.Form1
+            Me.MainForm = Global.HotelSystem.LoginPage
         End Sub
     End Class
 End Namespace
