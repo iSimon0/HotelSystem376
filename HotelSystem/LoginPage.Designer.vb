7e5373309f172e5d2eb79fca5f4c7f5875c11d6e
Ian Simon <isimon@umich.edu>
2019-04-23 18:41:29 -0400

    Final Changes to Hotel System

--- HotelSystem/LoginPage.Designer.vb
+++ HotelSystem/LoginPage.Designer.vb
@@ -29,32 +29,30 @@ Partial Class LoginPage
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.FailedLogin = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'UsernameBox
         '
-        Me.UsernameBox.Location = New System.Drawing.Point(418, 221)
-        Me.UsernameBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.UsernameBox.Location = New System.Drawing.Point(314, 180)
         Me.UsernameBox.Name = "UsernameBox"
-        Me.UsernameBox.Size = New System.Drawing.Size(281, 22)
+        Me.UsernameBox.Size = New System.Drawing.Size(212, 20)
         Me.UsernameBox.TabIndex = 0
         '
         'PasswordBox
         '
-        Me.PasswordBox.Location = New System.Drawing.Point(418, 254)
-        Me.PasswordBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.PasswordBox.Location = New System.Drawing.Point(314, 206)
         Me.PasswordBox.Name = "PasswordBox"
         Me.PasswordBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.PasswordBox.Size = New System.Drawing.Size(281, 22)
+        Me.PasswordBox.Size = New System.Drawing.Size(212, 20)
         Me.PasswordBox.TabIndex = 1
         Me.PasswordBox.UseSystemPasswordChar = True
         '
         'LoginButton
         '
-        Me.LoginButton.Location = New System.Drawing.Point(601, 286)
-        Me.LoginButton.Margin = New System.Windows.Forms.Padding(4)
+        Me.LoginButton.Location = New System.Drawing.Point(451, 232)
         Me.LoginButton.Name = "LoginButton"
-        Me.LoginButton.Size = New System.Drawing.Size(100, 28)
+        Me.LoginButton.Size = New System.Drawing.Size(75, 23)
         Me.LoginButton.TabIndex = 2
         Me.LoginButton.Text = "Enter"
         Me.LoginButton.UseVisualStyleBackColor = True
@@ -63,47 +61,53 @@ Partial Class LoginPage
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(339, 183)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(254, 149)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(62, 24)
+        Me.Label1.Size = New System.Drawing.Size(48, 18)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Log in"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(339, 225)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(254, 183)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(73, 17)
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Username"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(339, 258)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(254, 210)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(69, 17)
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Password"
+        '
+        'FailedLogin
+        '
+        Me.FailedLogin.AutoSize = True
+        Me.FailedLogin.Location = New System.Drawing.Point(311, 242)
+        Me.FailedLogin.Name = "FailedLogin"
+        Me.FailedLogin.Size = New System.Drawing.Size(10, 13)
+        Me.FailedLogin.TabIndex = 6
+        Me.FailedLogin.Text = " "
         '
         'LoginPage
         '
         Me.AcceptButton = Me.LoginButton
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FailedLogin)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.LoginButton)
         Me.Controls.Add(Me.PasswordBox)
         Me.Controls.Add(Me.UsernameBox)
-        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "LoginPage"
         Me.Text = "Hotel Master"
         Me.ResumeLayout(False)
@@ -117,4 +121,5 @@ Partial Class LoginPage
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents FailedLogin As Label
 End Class
--- HotelSystem/LoginPage.vb
+++ HotelSystem/LoginPage.vb
@@ -1,11 +1,31 @@
 ﻿Public Class LoginPage
     Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
-        If UsernameBox.Text = "name" Then
-            If PasswordBox.Text = "pass" Then
+        If UsernameBox.Text = "iSimon" Then
+            If PasswordBox.Text = "CIS376" Then
                 Dim form As New HomePage
                 form.Show()
                 Me.Close()
+            Else
+                FailedLogin.Text = "Invalid Login"
             End If
+        ElseIf UsernameBox.Text = "kMurdock" Then
+            If PasswordBox.Text = "CIS376" Then
+                Dim form As New HomePage
+                form.Show()
+                Me.Close()
+            Else
+                FailedLogin.Text = "Invalid Login"
+            End If
+        ElseIf UsernameBox.Text = "aSayad" Then
+            If PasswordBox.Text = "CIS376" Then
+                Dim form As New HomePage
+                form.Show()
+                Me.Close()
+            Else
+                FailedLogin.Text = "Invalid Login"
+            End If
+        Else
+            FailedLogin.Text = "Invalid Login"
         End If
 
     End Sub
